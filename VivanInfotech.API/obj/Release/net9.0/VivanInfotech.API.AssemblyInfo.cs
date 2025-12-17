2b72b8100fb27496b550b989a6bb96fc6c3feba5
Nikunj-Navadiya <nikunjnavadiya7@gmail.com>
2025-12-17 14:56:04 +0530

    Initial commit

--- VivanInfotech.API/obj/Release/net9.0/VivanInfotech.API.AssemblyInfo.cs
+++ VivanInfotech.API/obj/Release/net9.0/VivanInfotech.API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("VivanInfotech.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2b4d8076231ad0b9d51025e391800e9ac57c6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("VivanInfotech.API")]
